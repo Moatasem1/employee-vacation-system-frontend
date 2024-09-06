0082fe51b1719fbedb0a2e3dad95d1ac05b74eab
moatasem <moatasemalnaimat@gmail.com>
2024-09-06 20:49:08 +0300

    fix: active current page on netlify host

--- scripts/general.js
+++ scripts/general.js
@@ -16,15 +16,15 @@ async function printHeader() {
 function assignActiveToCurrentPage() {
     let currentUrl = window.location.pathname;
 
-    // If the current URL is just a forward slash, treat it as '/index.html'.
-    currentUrl = (currentUrl === "/") ? "/index.html" : currentUrl;
-
     const navLinks = document.querySelectorAll(".navbar .nav-link");
 
     for (const navLink of navLinks) {
         const navLinkUrl = navLink.getAttribute("href");
 
-        if (currentUrl === navLinkUrl) {
+        // If the current URL is just a forward slash, treat it as '/index.html'
+        if ((navLinkUrl == '/' && (currentUrl == '/' || currentUrl == '/index.html')) ||
+            currentUrl === navLinkUrl) {
+
             navLink.classList.add("active");
             break;
         }
